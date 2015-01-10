1357afb0deeffa1619026edc343f300b055ee716
other50g <other50g@live.jp>
2015-01-10 14:49:45 +0900

    ソースコードの更新

---
+++ TimeTable/TimeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by Sonoda Seiya on 2015/01/10.
+//  Copyright (c) 2015年 Sonoda Seiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
