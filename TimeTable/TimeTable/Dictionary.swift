d09ed1dfc53c7a4974a96488a23931ff2782c833
Yabiku11 <e135711@Yuuki-no-MacBook-Pro-2.local>
2015-01-15 10:47:23 +0900

    ボタンメソッドの実装

---
+++ TimeTable/TimeTable/Dictionary.swift
@@ -0,0 +1,11 @@
+//
+//  Dictionary.swift
+//  TimeTable
+//
+//  Created by Yuuki Yabiku on 2015/01/10.
+//  Copyright (c) 2015年 Sonoda Seiya. All rights reserved.
+//
+
+import Foundation
+
+var 
--- TimeTable/TimeTable/ViewController.swift
+++ TimeTable/TimeTable/ViewController.swift
@@ -19,7 +19,118 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func mon1(sender: AnyObject) {
+    }
 
+    @IBAction func mon2(sender: AnyObject) {
+    }
+    
+    @IBAction func mon3(sender: AnyObject) {
+    }
+
+    @IBAction func mon4(sender: AnyObject) {
+    }
+
+    @IBAction func mon5(sender: AnyObject) {
+    }
+
+    @IBAction func mon6(sender: AnyObject) {
+    }
+
+    @IBAction func mon7(sender: AnyObject) {
+    }
+
+    
+    @IBAction func tues1(sender: AnyObject) {
+    }
+    
+    @IBAction func tues2(sender: AnyObject) {
+    }
+
+    @IBAction func tues3(sender: AnyObject) {
+    }
+
+    @IBAction func tues4(sender: AnyObject) {
+    }
+    
+    @IBAction func tues5(sender: AnyObject) {
+    }
+    
+    @IBAction func tues6(sender: AnyObject) {
+    }
+    
+    @IBAction func tues7(sender: AnyObject) {
+    }
+
+  
+    @IBAction func wed1(sender: AnyObject) {
+    }
+    
+    @IBAction func wed2(sender: AnyObject) {
+    }
+    
+    @IBAction func wed3(sender: AnyObject) {
+    }
+
+    @IBAction func wed4(sender: AnyObject) {
+    }
+
+    @IBAction func wed5(sender: AnyObject) {
+    }
+    
+    @IBAction func wed6(sender: AnyObject) {
+    }
+    
+    @IBAction func wed7(sender: AnyObject) {
+    }
+   
+    
+    @IBAction func thurs1(sender: AnyObject) {
+    }
+    
+    @IBAction func thurs2(sender: AnyObject) {
+    }
+    
+    @IBAction func thurs3(sender: AnyObject) {
+    }
+
+    @IBAction func thurs4(sender: AnyObject) {
+    }
+    
+    @IBAction func thurs5(sender: AnyObject) {
+    }
+
+    @IBAction func thurs6(sender: AnyObject) {
+    }
+    
+    @IBAction func thurs7(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func fri1(sender: AnyObject) {
+    }
+    
+    @IBAction func fri2(sender: AnyObject) {
+    }
+    
+    @IBAction func fri3(sender: AnyObject) {
+    }
+    
+    @IBAction func fri4(sender: AnyObject) {
+    }
+    
+    @IBAction func fri5(sender: AnyObject) {
+    }
+    
+    @IBAction func fri6(sender: AnyObject) {
+    }
+    
+    @IBAction func fri7(sender: AnyObject) {
+    }
+    
+    
+    
 
 }
 
