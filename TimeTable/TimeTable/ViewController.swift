611760a88a62afa592f172e61454cd559d8f4b39
Yabiku11 <e135711@Yuuki-no-MacBook-Pro-2.local>
2015-01-21 15:53:56 +0900

    yabiku

--- TimeTable/TimeTable/ViewController.swift
+++ TimeTable/TimeTable/ViewController.swift
@@ -10,6 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mon1: UIButton!
+    @IBOutlet weak var mon2: UIButton!
+    @IBOutlet weak var mon3: UIButton!
+    @IBOutlet weak var mon4: UIButton!
+    @IBOutlet weak var mon5: UIButton!
+    @IBOutlet weak var mon6: UIButton!
+    @IBOutlet weak var mon7: UIButton!
+    @IBOutlet weak var tue1: UIButton!
+    @IBOutlet weak var tue2: UIButton!
+    @IBOutlet weak var tue3: UIButton!
+    @IBOutlet weak var tue4: UIButton!
+    @IBOutlet weak var tue5: UIButton!
+    @IBOutlet weak var tue6: UIButton!
+    @IBOutlet weak var tue7: UIButton!
+    @IBOutlet weak var wed1: UIButton!
+    @IBOutlet weak var wed2: UIButton!
+    @IBOutlet weak var wed3: UIButton!
+    @IBOutlet weak var wed4: UIButton!
+    @IBOutlet weak var wed5: UIButton!
+    @IBOutlet weak var wed6: UIButton!
+    @IBOutlet weak var wed7: UIButton!
+    @IBOutlet weak var thu1: UIButton!
+    @IBOutlet weak var thu2: UIButton!
+    @IBOutlet weak var thu3: UIButton!
+    @IBOutlet weak var thu4: UIButton!
+    @IBOutlet weak var thu5: UIButton!
+    @IBOutlet weak var thu6: UIButton!
+    @IBOutlet weak var thu7: UIButton!
+    @IBOutlet weak var fri1: UIButton!
+    @IBOutlet weak var fri2: UIButton!
+    @IBOutlet weak var fri3: UIButton!
+    @IBOutlet weak var fri4: UIButton!
+    @IBOutlet weak var fri5: UIButton!
+    @IBOutlet weak var fri6: UIButton!
+    @IBOutlet weak var fri7: UIButton!
+    
+    
+    @IBOutlet weak var kara: UIButton!
+    @IBOutlet weak var mon: UIButton!
+    @IBOutlet weak var tue: UIButton!
+    @IBOutlet weak var wed: UIButton!
+    @IBOutlet weak var thu: UIButton!
+    @IBOutlet weak var fri: UIButton!
+    
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var fonr: UIButton!
+    @IBOutlet weak var five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var seven: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +70,152 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func kara(sender: AnyObject) {
+    }
+    @IBAction func mon(sender: AnyObject) {
+    }
+    @IBAction func tue(sender: AnyObject) {
+    }
+    @IBAction func wed(sender: AnyObject) {
+    }
+    @IBAction func thu(sender: AnyObject) {
+    }
+    @IBAction func fri(sender: AnyObject) {
+    }
+    
+    @IBAction func one(sender: AnyObject) {
+    }
+    @IBAction func two(sender: AnyObject) {
+    }
+    @IBAction func three(sender: AnyObject) {
+    }
+    @IBAction func fonr(sender: AnyObject) {
+    }
+    @IBAction func five(sender: AnyObject) {
+    }
+    @IBAction func six(sender: AnyObject) {
+    }
+    @IBAction func seven(sender: AnyObject) {
+    }
 
+    
+    
+    
+    
+    
+    @IBAction func mon1(sender: AnyObject) {
+    }
 
+    @IBAction func mon2(sender: AnyObject) {
+    }
+
+    @IBAction func mon3(sender: AnyObject) {
+    }
+    
+    @IBAction func mon4(sender: AnyObject) {
+    }
+
+    @IBAction func mon5(sender: AnyObject) {
+    }
+    
+    @IBAction func mon6(sender: AnyObject) {
+    }
+  
+    @IBAction func mon7(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func tue1(sender: AnyObject) {
+    }
+    
+    @IBAction func tue2(sender: AnyObject) {
+    }
+    
+    @IBAction func tue3(sender: AnyObject) {
+    }
+    
+    @IBAction func tue4(sender: AnyObject) {
+    }
+    
+    @IBAction func tue5(sender: AnyObject) {
+    }
+    
+    @IBAction func tue6(sender: AnyObject) {
+    }
+    
+    @IBAction func tue7(sender: AnyObject) {
+    }
+    
+    
+    
+    @IBAction func wed1(sender: AnyObject) {
+    }
+
+    @IBAction func wed2(sender: AnyObject) {
+    }
+    
+    @IBAction func wed3(sender: AnyObject) {
+    }
+    
+    @IBAction func wed4(sender: AnyObject) {
+    }
+    
+    @IBAction func wed5(sender: AnyObject) {
+    }
+    
+    @IBAction func wed6(sender: AnyObject) {
+    }
+
+    @IBAction func wed7(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func thu1(sender: AnyObject) {
+    }
+    
+    @IBAction func thu2(sender: AnyObject) {
+    }
+    
+    @IBAction func thu3(sender: AnyObject) {
+    }
+    
+    @IBAction func thu4(sender: AnyObject) {
+    }
+    
+    @IBAction func thu5(sender: AnyObject) {
+    }
+    
+    @IBAction func thu6(sender: AnyObject) {
+    }
+    
+    @IBAction func thu7(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func fri1(sender: AnyObject) {
+    }
+    
+    @IBAction func fri2(sender: AnyObject) {
+    }
+    
+    @IBAction func fri3(sender: AnyObject) {
+    }
+    
+    @IBAction func fri4(sender: AnyObject) {
+    }
+    
+    @IBAction func fri5(sender: AnyObject) {
+    }
+    
+    @IBAction func fri6(sender: AnyObject) {
+    }
+
+    @IBAction func fri7(sender: AnyObject) {
+    }
+   
+    
+    
 }
-
